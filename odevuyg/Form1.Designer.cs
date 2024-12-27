0d89e9b4dd8514c4dff7b77ac6f7f6dbf76d9bf2
Sait Şahin <76446815+saittaha05@users.noreply.github.com>
2024-12-28 01:17:30 +0300

    Add files via upload

---
+++ odevuyg/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace odevuyg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsprs = new System.Windows.Forms.Button();
+            this.btnurun = new System.Windows.Forms.Button();
+            this.btnorders = new System.Windows.Forms.Button();
+            this.btnchai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsprs
+            // 
+            this.btnsprs.Location = new System.Drawing.Point(25, 116);
+            this.btnsprs.Name = "btnsprs";
+            this.btnsprs.Size = new System.Drawing.Size(181, 114);
+            this.btnsprs.TabIndex = 0;
+            this.btnsprs.Text = "AFKI SİPARİŞLER";
+            this.btnsprs.UseVisualStyleBackColor = true;
+            this.btnsprs.Click += new System.EventHandler(this.btnsprs_Click);
+            // 
+            // btnurun
+            // 
+            this.btnurun.Location = new System.Drawing.Point(212, 116);
+            this.btnurun.Name = "btnurun";
+            this.btnurun.Size = new System.Drawing.Size(181, 114);
+            this.btnurun.TabIndex = 1;
+            this.btnurun.Text = "Ürünler tablosunda en yüksek fiyata sahip ürünün id ve isim bilgisini bulalım";
+            this.btnurun.UseVisualStyleBackColor = true;
+            this.btnurun.Click += new System.EventHandler(this.btnurun_Click);
+            // 
+            // btnorders
+            // 
+            this.btnorders.Location = new System.Drawing.Point(399, 116);
+            this.btnorders.Name = "btnorders";
+            this.btnorders.Size = new System.Drawing.Size(181, 114);
+            this.btnorders.TabIndex = 2;
+            this.btnorders.Text = "Siparişler tablosunda 20 ile 50.siparişlerin bilgilerini bulalım,";
+            this.btnorders.UseVisualStyleBackColor = true;
+            this.btnorders.Click += new System.EventHandler(this.btnorders_Click);
+            // 
+            // btnchai
+            // 
+            this.btnchai.Location = new System.Drawing.Point(586, 116);
+            this.btnchai.Name = "btnchai";
+            this.btnchai.Size = new System.Drawing.Size(181, 114);
+            this.btnchai.TabIndex = 3;
+            this.btnchai.Text = "Chai adlı ürünün tüm sipariş detaylarını listeleyelim";
+            this.btnchai.UseVisualStyleBackColor = true;
+            this.btnchai.Click += new System.EventHandler(this.btnchai_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnchai);
+            this.Controls.Add(this.btnorders);
+            this.Controls.Add(this.btnurun);
+            this.Controls.Add(this.btnsprs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsprs;
+        private System.Windows.Forms.Button btnurun;
+        private System.Windows.Forms.Button btnorders;
+        private System.Windows.Forms.Button btnchai;
+    }
+}
+
---
+++ odevuyg/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odevuyg
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db = new NorthwindEntities(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ***ALFKI müşteri ID'sine sahip müşterinin siparişlerini listeleyelim,
+
+            //* **Ürünler tablosunda en yüksek fiyata sahip ürünün id ve isim bilgisini bulalım,
+
+            //***Siparişler tablosunda 20 ile 50.siparişlerin bilgilerini bulalım,
+
+            //***Chai adlı ürünün tüm sipariş detaylarını listeleyelim
+
+
+        }
+
+        private void btnsprs_Click(object sender, EventArgs e)
+        {
+            var data = db.Orders
+                         .Where(x => x.CustomerID == "ALFKI")
+                         .OrderBy(x => x.OrderID)
+                         //.Select(x => x)
+                         //.Select(x => x.CompanyName)
+                         .Select(x => new
+                         {
+                            siparisnumarasi = x.OrderID,
+                             musterinumarasi = x.CustomerID,
+                             calisan = x.EmployeeID,
+                             siparistarihi= x.OrderDate,
+                         })
+                         .ToList();
+
+            foreach (var item in data)
+            {
+                MessageBox.Show("sipariş numarası " + item.siparisnumarasi+ "\n"+
+                    "Müşteri numarası "+item.musterinumarasi+"\n"+"calisan "+item.calisan +"\n"+ "sipariş tarihi"+item.siparistarihi);
+            }
+        }
+
+        private void btnurun_Click(object sender, EventArgs e)
+        {
+            var data = db.Products.OrderByDescending(x => x.UnitPrice).First();
+            MessageBox.Show("Ürün ID: " + data.ProductID +"\n"+ "Ürün Adı : "+ data.ProductName);
+
+        }
+
+        private void btnorders_Click(object sender, EventArgs e)
+        {
+            var data = db.Orders.OrderBy(x => x.OrderID).Skip(20).Take(50).ToList();
+            foreach (var item in data) 
+            {
+                MessageBox.Show("Sipariş numarası : " + item.OrderID + "\n" + "Müşteri Adı :" + item.ShipName);
+            }
+        }
+
+        private void btnchai_Click(object sender, EventArgs e)
+        {
+            var data= db.Order_Details
+                .Join(db.Products,
+                od => od.ProductID,
+                p=> p.ProductID,
+                (od, p)=> new {Order_Details = od, Products = p})
+                .Where(x => x.Products.ProductName=="Cahi")
+                .Select(x=> new 
+                {
+                    OrderID = x.Order_Details.OrderID,
+                    ProductName = x.Products.ProductName,
+                    UnitPrice = x.Order_Details.UnitPrice,
+
+                })
+                .ToList();
+            foreach(var item in data) 
+            {
+                MessageBox.Show("Ürün Adı : " + item.ProductName + "\n" +"Sipariş numarasi"+ item.OrderID  + "\n"+"Ürün fiyatı :" + item.UnitPrice);
+                
+
+            }
+        }
+    }
+}
